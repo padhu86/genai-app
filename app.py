592ac343675b755a6f6959722df5de9c09298a7f
padhu86 <37106046+padhu86@users.noreply.github.com>
2026-03-27 18:33:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def home():
-    return {"message": "App is working"}
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/chat")
+async def chat(req: Request):
+    body = await req.json()
+    user_query = body["message"]
+
+    return {"response": f"You said: {user_query}"}
