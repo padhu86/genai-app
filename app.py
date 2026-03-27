0ab7dc5436933a616675f37b01de5c9a638ac3b0
padhu86 <37106046+padhu86@users.noreply.github.com>
2026-03-27 18:47:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+
+# Fix for Azure path issue
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
